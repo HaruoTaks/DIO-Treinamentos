412d61967c7014f0766b4953ac13075622ebdccd
HaruoTaks <haruo.takahashi@outlook.com>
2022-05-17 13:50:58 -0300

    Módulos

---
+++ DIO-PHILIPS-BOOTCAMP/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Rodrigo", 30));
+console.log(mostraCidade("Rodrigo", "Diadema"));
+console.log(mostraHobby("Rodrigo", "Jogar videogame"));
