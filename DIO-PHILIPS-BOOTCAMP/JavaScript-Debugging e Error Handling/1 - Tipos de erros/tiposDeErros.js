918f2e9f4c32957ba051974ed3337c2b7e1462dc
HaruoTaks <haruo.takahashi@outlook.com>
2022-05-25 12:47:44 -0300

    Javascript Debugging e Error Handling - Tipos de erros

---
+++ DIO-PHILIPS-BOOTCAMP/JavaScript-Debugging e Error Handling/1 - Tipos de erros/tiposDeErros.js
@@ -0,0 +1,13 @@
+// objeto Error do ECMAScript
+// Erros que acontece em tempo de execução
+/* Composto por:
+    - Mensagem
+    - Nome
+    - Linha
+    - Call Stack */
+
+
+//-----------------------------------------
+// DOMException
+// Erros relacionado ao Document Object Model (DOM)
+// Erro refrentes a estrutura de uma árvore de elementos dentro de um página na web
