bf14e22dd8963499a7b4747b93689dc15d722156
HaruoTaks <haruo.takahashi@outlook.com>
2022-05-25 21:12:13 -0300

    Javascript - Orientação a Objetos - Paradigmas e Pilares

---
+++ DIO-PHILIPS-BOOTCAMP/Javascript-Orientacao a objetos/1 - Paradigmas e Pilares/index.js
@@ -0,0 +1,32 @@
+// Paradigmas
+// Imperativo - Como resolver os problemas
+/*Tipos:
+    - Procedural
+    - Orientada a objeto
+    - Processo Paralelo
+ */
+// Declarativo - No que você vai resolver
+/*Tipos:
+    - Banco de dados
+    - Programação funcional
+    - Lógica
+    - Fluxo de dados
+*/
+
+// Programa - "Objetos" que possuem uma série de propriedades
+/*Pilares:
+    - Herança
+    - Polimorfismo
+    - Encapsulamento
+    - Abstração 
+*/
+/* Abstração - Processo mental que consiste em isolar um aspecto determinado de um 
+estado de coisas relativamente complexo, a fim de simplificar a sua avaliação,
+classificação ou para permitir a comunicação do mesmo. */
+
+// Herança -  O objeto filho herda propriedades e métodos do objeto pai.
+
+// Ecapsulamento - Cada classe tem propriedades e métodos independentes do restante do código
+
+/* Polimorfismo - Objetos podem herdar a mesma classe pai, 
+mas se comportarem de forma diferente quando invocamos seu métodos*/
