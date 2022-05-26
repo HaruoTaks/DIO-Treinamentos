efcd34ad9d7f72892faffb48e44b6e6945159ecd
HaruoTaks <haruo.takahashi@outlook.com>
2022-05-26 13:33:16 -0300

    Javascript - Orientação a Objetos - protótipos e classes

---
+++ DIO-PHILIPS-BOOTCAMP/Javascript-Orientacao a objetos/2 - Protótipos e Classes/index.js
@@ -0,0 +1,10 @@
+/* Protótipos: 
+    - Todos os objetos Javascript herdam propriedades e métodos de um prototype.
+    - O objeto Object.prototype está no topo desta cadeia
+*/
+
+/* Classes:
+    - Syntatic sugar: uma sintaxe feita para facilitar a escrita
+    - Javascipt não possui classes nativamente. Todas as classes são objetos e a herança se dá
+    por protótipoa.
+ */ 
